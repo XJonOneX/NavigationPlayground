7acc2a10ca3af245411a5010d53121bef8863991
Jon Harmon <JonDenverCO@gmail.com>
2022-10-24 19:58:51 -0400

    Changed initial behavior of flyout menu to use what is defined in the xaml for page links.

--- NavigationPlayground/FlyoutPageDemo.xaml.cs
+++ NavigationPlayground/FlyoutPageDemo.xaml.cs
@@ -16,14 +16,14 @@ public partial class FlyoutPageDemo : FlyoutPage
 			//flyoutMenuPage.IsPresented = false;
 			//Navigation.PushAsync(new Page2());
 
-			Detail = new NavigationPage(new Page2());
+			//Detail = new NavigationPage(new Page2());
 
 			//Application.Current.MainPage.Navigation.PushAsync(new Page2());
 			//Navigation.PushAsync(new NavigationPage((Page)Activator.CreateInstance(item.TargetType)));
 
 			//This line works, but doesn't display the back arrow
 			//I'm asuuming because it wasn't done with Navigation.PushAsync
-			//Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+			Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
 		}
 	}
 }
